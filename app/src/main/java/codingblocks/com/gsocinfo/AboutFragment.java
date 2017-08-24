634a1e2f91955cd53acf9c77c0ff2e7c8e2e5978
the-dagger <harshithdwivedi@gmail.com>
2017-08-25 04:12:55 +0530

    feat : add nav drawer

---
+++ app/src/main/java/codingblocks/com/gsocinfo/AboutFragment.java
@@ -0,0 +1,28 @@
+package codingblocks.com.gsocinfo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by harshit on 25/08/17.
+ */
+
+public class AboutFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_about,container,false);
+
+        RecyclerView recyclerView = view.findViewById(R.id.rv_timeline);
+
+
+        return view;
+//        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
