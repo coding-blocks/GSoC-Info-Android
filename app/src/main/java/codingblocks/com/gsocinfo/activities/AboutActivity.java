5fc07a538c1905a0d74bcc90a52e0b02f84bed50
the-dagger <harshithdwivedi@gmail.com>
2017-08-25 17:23:49 +0530

    feat : add faq page with animations

--- app/src/main/java/codingblocks/com/gsocinfo/activities/AboutActivity.java
+++ app/src/main/java/codingblocks/com/gsocinfo/activities/AboutActivity.java
@@ -2,9 +2,8 @@ package codingblocks.com.gsocinfo.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,9 +11,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-import codingblocks.com.gsocinfo.fragments.AboutFragment;
 import codingblocks.com.gsocinfo.R;
+import codingblocks.com.gsocinfo.fragments.AboutFragment;
+import codingblocks.com.gsocinfo.fragments.FaqFragment;
 
 public class AboutActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -94,7 +95,7 @@ public class AboutActivity extends AppCompatActivity
         } else if (id == R.id.nav_projects) {
 
         } else if (id == R.id.nav_faq) {
-
+            getSupportFragmentManager().beginTransaction().replace(R.id.container_about, FaqFragment.newInstance()).commit();
         } else if (id == R.id.nav_chat) {
 
         }
