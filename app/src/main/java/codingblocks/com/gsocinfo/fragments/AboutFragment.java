07cda1a08ceb237750ec5ffccb917f2d62cbaf30
the-dagger <harshithdwivedi@gmail.com>
2017-09-01 15:13:18 +0530

    feat : implement viewFlipper in about page

--- app/src/main/java/codingblocks/com/gsocinfo/fragments/AboutFragment.java
+++ app/src/main/java/codingblocks/com/gsocinfo/fragments/AboutFragment.java
@@ -9,10 +9,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.daimajia.slider.library.Indicators.PagerIndicator;
-import com.daimajia.slider.library.SliderLayout;
-import com.daimajia.slider.library.SliderTypes.DefaultSliderView;
-
 import codingblocks.com.gsocinfo.R;
 import codingblocks.com.gsocinfo.adapters.TimelineAdapter;
 
@@ -22,8 +18,6 @@ import codingblocks.com.gsocinfo.adapters.TimelineAdapter;
 
 public class AboutFragment extends Fragment {
 
-
-    SliderLayout sliderLayout;
     public static AboutFragment newInstance() {
 
         Bundle args = new Bundle();
@@ -42,23 +36,6 @@ public class AboutFragment extends Fragment {
         RecyclerView recyclerView = view.findViewById(R.id.rv_timeline);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
-
-        sliderLayout = view.findViewById(R.id.sliderView);
-
-        DefaultSliderView apply  = new DefaultSliderView(getContext());
-
-        apply.image(R.drawable.apply);
-        DefaultSliderView code = new DefaultSliderView(getContext());
-        code.image(R.drawable.code);
-        DefaultSliderView share = new DefaultSliderView(getContext());
-        share.image(R.drawable.share);
-
-        sliderLayout.addSlider(apply);
-        sliderLayout.addSlider(code);
-        sliderLayout.addSlider(share);
-        sliderLayout.setIndicatorVisibility(PagerIndicator.IndicatorVisibility.Invisible);
-        sliderLayout.startAutoCycle();
-
         TimelineAdapter timelineAdapter = new TimelineAdapter();
 
 
@@ -66,9 +43,4 @@ public class AboutFragment extends Fragment {
         return view;
     }
 
-    @Override
-    public void onStop() {
-        sliderLayout.stopAutoCycle();
-        super.onStop();
-    }
 }
