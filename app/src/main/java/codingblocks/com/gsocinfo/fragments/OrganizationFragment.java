43a53f1c811cb0781cc34dfa7b8b1e40011d23c7
Harshit Dwivedi <harshithdwivedi@gmail.com>
2017-10-23 13:11:28 +0530

    refactor (#8)
    Signed-off-by: the-dagger <harshithdwivedi@gmail.com>

--- app/src/main/java/codingblocks/com/gsocinfo/fragments/OrganizationFragment.java
+++ app/src/main/java/codingblocks/com/gsocinfo/fragments/OrganizationFragment.java
@@ -10,7 +10,6 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
